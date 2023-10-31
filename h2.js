9ae1859bcd10e67a7a4ede1c1d57918c1b95c58b
sucaund <hsh7143@gmail.com>
2023-10-31 18:35:48 +0900

    1031

--- h2.js
+++ h2.js
@@ -17,7 +17,71 @@ const dataA={
     
     eur:["4","돈까스","함박스테끼","생선가스"],
     eurImg:["","img0.jpg","img2.jpg","img20.jpg"],
-    eurprice:[0,8000,5500,4000]
+    eurprice:[0,8000,5500,4000],
+
+    shop:[
+        "",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1329.8222169496646!2d126.9441246459347!3d37.55817163965985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c989aee23ef45%3A0x952694d6486aca05!2z7LKc7KeE67aE7Iud!5e0!3m2!1sko!2skr!4v1698738437558!5m2!1sko!2skr",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25302.91123251091!2d126.92460770327409!3d37.55827011675357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357c99e150e9ad9b%3A0x34bba933e6fc3991!2z7Jyh7IiY64u5IOydtOuMgOyXreygkA!5e0!3m2!1sko!2skr!4v1698739814037!5m2!1sko!2skr",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25302.889342252132!2d126.92460769547247!3d37.558334579376485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca3e38c8be179%3A0x3319f633a1f5edb!2z7KSR7ZmU6rCA7KCV!5e0!3m2!1sko!2skr!4v1698739955832!5m2!1sko!2skr",
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25302.867451982034!2d126.92460768767079!3d37.5583990419384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca34c5c49232b%3A0x75e73188c2212ecb!2z7J6Q6rCI7LmY!5e0!3m2!1sko!2skr!4v1698740189201!5m2!1sko!2skr"
+        ],
+    
+    tip_1A :["","값에비해 양이 많다","맛이 있다."], //천진분식
+    tip_2A :["","얼큰한맛","국물이 맛이 있다."], //육수당
+    tip_3A :["","짬뽕","짜장"], //중화
+    tip_4A :["","값에비해 양이 적다","맛이 제법 있다."], //자갈치
+    }
+
+
+
+
+function func_ch(js){
+    //no는 콤보박스에서 선택된 번호 (0부터 시작)
+    no=js.selectedIndex;
+    $(".box0").text(dataA.kind[no]+" 선택");
+    $(".ch")
+    kind_func(no);   
+}
+
+function kind_func(js){
+    if(js==1){
+        for(i=1;i<dataA.kor.length;i++){
+            $(".box"+i).text(dataA.kor[i]);
+        }
+        for(i=1;i<dataA.tip_1A.length;i++){
+            $(".tip"+i).html(dataA.tip_1A[i]);
+        }
+    }
+
+    else if(js==2){
+        for(i=1;i<dataA.bun.length;i++){
+            $(".box"+i).text(dataA.bun[i]);
+        }
+        for(i=1;i<dataA.tip_1A.length;i++){
+            $(".tip"+i).html(dataA.tip_2A[i]);
+        }
+    }
+
+    else if(js==3){
+        for(i=1;i<dataA.cha.length;i++){
+            $(".box"+i).text(dataA.cha[i]);
+        }
+        for(i=1;i<dataA.tip_1A.length;i++){
+            $(".tip"+i).html(dataA.tip_3A[i]);
+        }
+    }
+
+    else if(js==4){
+        for(i=1;i<dataA.eur.length;i++){
+            $(".box"+i).text(dataA.eur[i]);
+        }
+        for(i=1;i<dataA.tip_1A.length;i++){
+            $(".tip"+i).html(dataA.tip_4A[i]);
+        }
+    }
+    $(".wrap3 li").css("background","white").css("color","black");
+    $(".box0").css("color","blue");
 }
 
 
@@ -29,6 +93,7 @@ $(function(){
     $(".wrap3 li"+count).css("background","white").css("color","black"); //모든 li는 원래 배경
     
     $(".box"+count).css("background","black").css("color","white"); //선택 배경
+    
     if(no==1){
         $(".pic").attr("src","img/"+dataA.korImg[count]);
         $(".text").text(dataA.korprice[count]+"원");
@@ -51,38 +116,8 @@ $(function(){
     //다시하기 버튼을 누르면 count와 콤보박스(no) 취소가 초기화
     $(".again").on("click",function(){
         count=0; no=0;
-        $(".wrap3 li").text("").css("background","white").css("color","black");
+        $(".wrap3 li").text("")
+                        .css("background","white")
+                        .css("color","black");
     })
 })
-
-function func_ch(js){
-    no=js.selectedIndex;
-    $(".box0").text(dataA.kind[no]+" 선택");
-    kind_func(no);   
-}
-
-function kind_func(js){
-    if(js==1){
-        for(i=1;i<dataA.kor.length;i++){
-            $(".box"+i).text(dataA.kor[i]);
-        }
-    }
-
-    if(js==2){
-        for(i=1;i<dataA.bun.length;i++){
-            $(".box"+i).text(dataA.bun[i]);
-        }
-    }
-
-    if(js==3){
-        for(i=1;i<dataA.cha.length;i++){
-            $(".box"+i).text(dataA.cha[i]);
-        }
-    }
-
-    if(js==4){
-        for(i=1;i<dataA.eur.length;i++){
-            $(".box"+i).text(dataA.eur[i]);
-        }
-    }
-}
--- js/sec4.js
+++ js/sec4.js
@@ -1,11 +1,51 @@
 // 섹션4(모임) 탑다운메뉴의 주메뉴 (ul-li)의 색상지정
-
-
 const sec4_dataA={
     menuColor : ["tomato","skyblue","lightgreen","#ccc","lightyellow"]
+    
+    ,sec4_smallA :[
+        "img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg"]
+    ,sec4_smallA_back4 :[
+        "img1.jpg","img2.jpg","img3.jpg","img4.jpg","img5.jpg","img6.jpg"]    
+    ,sec4_small_textA : [
+        "그림설명1 <br> 숫자 : 10 <br>good",
+        "그림설명2 <br> 숫자 : 10 <br>good",
+        "그림설명3 <br> 숫자 : 10 <br>good",
+        "그림설명4 <br> 숫자 : 10 <br>good",
+        "그림설명5 <br> 숫자 : 10 <br>good",
+        "그림설명6 <br> 숫자 : 10 <br>good"
+    ]    ,
+    sec4_headA:[ 
+        "1 첫화면",
+        "2 특산품",
+        "3 기념음식",
+        "3 맛집소개",
+        "4 착한식당",
+        "5 볼거리1",
+        "6 볼거리2",
+        "7 볼거리3",
+        "8 재미거리1",
+        "9 재미거리2",
+        "10 참여마당1",
+        "11 참여마당2",
+        "12 참여마당3",
+        "13 아카데미1",
+        "14 아카데미2"
+    ]
 }
 
+
+
+//섹션4 시작하자마자 모든 아티클 안보이그 at0만보이기
+$(".sec4_at").hide();
+$(".sec4_at0").show();
+// 섹션안에 모든아티클속에 p(.sec_head번호)안에 선택서브
+for(i=0; i<sec4_dataA.sec4_headA.length;i++){
+    $(".sec4_head"+i).html(sec4_dataA.sec4_headA[i]);
+}
+
+//섹션4
 // 섹션4의 메뉴 5개에 주메뉴(p)에 색상넣기
+// ================================펑션 일반 문=================================
 $(function(){
 
     for(i=0; i<sec4_dataA.menuColor.length;i++){
@@ -13,4 +53,42 @@ $(function(){
                                         .css("opacity","0.8");
     }
 
+
+// 하단 그림,글자넣기
+    for(i=0;i<sec4_dataA.sec4_smallA.length;i++){
+    
+        $(".sec4_sm_img"+i)
+            .css("background","url(img/"+ sec4_dataA.sec4_smallA[i]+")")
+            .css("backgroundSize","100% 100%")
+        $(".sec_sm_img_text"+i).html(sec4_dataA.sec4_small_textA[i]);
+    }
+    // 요런 방법도 있고
+        //     $(".sec4_small_img li").eq(i)
+    //     .css("background","url(img/"+ sec4_dataA.sec4_smallA[i]+")")
+    //     .css("backgroundSize","100% 100%");
+    // 작은그림 오버하면 배경변환
+    $(".sec4_small_img li div").hover(function(){
+        no=$(this).attr("class").substring(11,12);
+        console.log(no);
+        $(".sec4_sm_img"+no)
+        .css("background","url(img/"+ sec4_dataA.sec4_smallA_back4[i]+")")
+        .css("backgroundSize","100% 100%")
+    })
+    
 })
+// =============================펑션 끝============================================
+
+
+// ===========서브메뉴 클릭시 해당아티클이 나타나야함=================================
+$(".sec4_subMenu").on("click",function(){
+    no=$(this).attr("class").substring(12,14)
+    console.log(no);
+});
+
+
+
+
+
+
+
+
